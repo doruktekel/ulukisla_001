d4320d3aa81a2843965dd684109522f6c38601c9
doruk.tekel <doruktekel55@gmail.com>
2025-09-26 16:48:21 +0300

    added slug page

---
+++ src/app/[...slug]/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import MainSlider from "../components/MainSlider";
+
+const SlugPage = () => {
+  return (
+    <div className="min-h-screen">
+      <MainSlider />
+    </div>
+  );
+};
+
+export default SlugPage;
