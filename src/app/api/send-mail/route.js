808462c548b94fcd238540b5a7f90558dc42b918
doruk.tekel <doruktekel55@gmail.com>
2025-10-01 09:08:55 +0300

    changed smtp port

--- src/app/api/send-mail/route.js
+++ src/app/api/send-mail/route.js
@@ -36,10 +36,13 @@ export async function POST(req) {
     const transporter = nodemailer.createTransport({
       host: process.env.SMTP_HOST,
       port: process.env.SMTP_PORT,
-      secure: true, // true for 465, false for other ports
+      secure: false, // true for 465, false for other ports
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
+      },
+      tls: {
+        rejectUnauthorized: false, // hosting sertifikası self-signed ise gerekebilir
       },
     });
 
