d1dea45269e4ac788f85447a2d1ebc1af5b0bc54
doruk.tekel <doruktekel55@gmail.com>
2025-10-06 15:23:49 +0300

    added new logo and new favicon

--- src/app/components/Slide1.jsx
+++ src/app/components/Slide1.jsx
@@ -8,15 +8,14 @@ gsap.registerPlugin(SplitText);
 
 const Slide1 = () => {
   useGSAP(() => {
-    let split = SplitText.create(".split", { type: "lines" });
-
-    gsap.from(split.lines, {
+    gsap.from(".logo", {
       duration: 1,
       y: 100,
       autoAlpha: 0,
       stagger: 0.5,
       ease: "power4.out",
       delay: 2,
+      scale: 0.6,
     });
   });
 
@@ -36,21 +35,21 @@ const Slide1 = () => {
       </video>
 
       {/* Overlay */}
-      <div className="absolute inset-0 bg-black/10 translate-0 pt-4 transition-opacity duration-700">
-        <div className="text-white text-center">
+      <div className="absolute inset-0 bg-black/10 translate-0 transition-opacity duration-700 flex justify-center items-center ">
+        <div className="text-white text-center logo">
           <Image
             src="/logo_b.webp"
             alt="Logo"
-            width={300}
-            height={300}
-            className="mx-auto mb-4"
+            width={200}
+            height={200}
+            className="mx-auto mb-4 w-[250px] md:w-[500px] "
           />
-          <h1 className="md:text-5xl text-2xl font-bold mb-2 header split">
+          {/* <h1 className="md:text-5xl text-2xl font-bold mb-2 header split">
             ULUKIŞLA ANADOLU
           </h1>
           <p className="md:text-5xl text-2xl mb-2 content split font-bold">
             YAPI KOOPERATİFİ
-          </p>
+          </p> */}
         </div>
       </div>
     </div>
